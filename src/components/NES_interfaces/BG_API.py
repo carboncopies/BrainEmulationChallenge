0fc8559fad11555914de1d2195d693df0c1e6bc4
Randal A. Koene <rkoene@carboncopies.org>
2023-06-24 15:21:28 -0700

    Updated BG_API to attempt Sphere creation as test.

--- src/components/NES_interfaces/BG_API.py
+++ src/components/NES_interfaces/BG_API.py
@@ -9,7 +9,7 @@ BrainGenix API.
 
 import requests
 
-BGAPI_BASE_URI='http://api.braingenix.org:8000'
+BGAPI_BASE_URI='http://api.braingenix.org:80'
 
 def API_call_raw(uri:str)->requests.Response:
     '''
@@ -35,9 +35,39 @@ def BGNES_simulation_create(name:str)->str:
     else:
         raise Exception('BGNES_simulation_create: Failed with GET status %s.' % response.status_code)
 
+def BGNES_sphere_create(radius_nm:float, center_nm:tuple, name=None)->str:
+    if name is None:
+        response = BGAPI_call('/NES/Geometry/Shape/Sphere/Create?Radius_nm=%s&Center_nm=%s' % (
+            str(radius_nm),
+            str(center_nm)
+            ))
+    else:
+        response = BGAPI_call('/NES/Geometry/Shape/Sphere/Create?Radius_nm=%s&Center_nm=%s&Name=%s' % (
+            str(radius_nm),
+            str(center_nm),
+            str(name)
+            ))
+    if response.status_code==200:
+        try:
+            data = response.json()
+            if data['StatusCode']==0:
+                return data['ShapeID']
+            else:
+                raise Exception('BGNES_sphere_create: API returned status code %s.' % data['StatusCode'])
+        except Exception as e:
+            raise Exception('BGNES_sphere_create: API did not return expected JSON data.')
+    else:
+        raise Exception('BGNES_sphere_create: Failed with GET status %s.' % response.status_code)
+
+
 # -- Testing API calls: -----------------------------------------
 
 if __name__ == '__main__':
+    
+    print('Calling BGNES_sphere_create...')
+    sphere_id = BGNES_sphere_create(10, (0,0,0))
+    print('Shape ID: '+str(sphere_id))
+
     print('Calling BGNES_simulation_create...')
     simulation_id = BGNES_simulation_create('test')
     print('Simulation ID: '+str(simulation_id))
