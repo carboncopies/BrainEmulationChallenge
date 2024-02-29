381642ff7b8825001ac6ee972a81f6b988bf193d
Thomas Liao <tliao@carboncopies.org>
2024-02-28 19:51:57 -0800

    Update Demo Scropt

--- src/models/e0_bs/Demo.sh
+++ src/models/e0_bs/Demo.sh
@@ -9,7 +9,7 @@ echo "Firstly, creating simulation on NES"
 ./bs_vbp00_groundtruth_xi_sampleprep.py
 
 echo "Now, running data acquisition step"
-./bs_vbp01_doubleblind_x_acquisition.py -RenderVisualization -RenderEM
+./bs_vbp01_doubleblind_x_acquisition.py -RenderVisualization -RenderEM -RenderCA
 
 echo "Creating Visualization GIF"
 ffmpeg -i "Renders/Visualizations/%d.png" Renders/Visualizations/Spin.gif -y
