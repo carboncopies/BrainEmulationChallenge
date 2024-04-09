bb5b9336cdc59b1b6ad3a8deb1696a9160297890
Thomas Liao <tliao@carboncopies.org>
2024-04-09 15:08:14 -0700

    Update Visualizer

--- Challenge/SubmissionConverter/VisualizerTest.py
+++ Challenge/SubmissionConverter/VisualizerTest.py
@@ -1,8 +1,7 @@
 #!/bin/python3
 
 import argparse
-import json
-import datetime
+import math
 
 import BrainGenix
 import BrainGenix.NES as NES
@@ -13,7 +12,19 @@ Parser = argparse.ArgumentParser(description="Submission Conversion Format For T
 Parser.add_argument("-Remote", action='store_true', help="Build Converted Model On Remote NES Server Rather Than Localhost")
 Parser.add_argument("-i", required=True, help="Path To Input Filename")
 Parser.add_argument("-o", required=True, help="Path To Directories Where Images Will Be Saved")
+Parser.add_argument("--CameraDistance", default=50, help="Distance in micrometers from the world origin to rotate the camera around")
+Parser.add_argument("--CameraHeight", default=50, help="Sets the height of the camera on the z axis from the world origin")
+Parser.add_argument("--NumSteps", default=10, help="Sets the number of steps to orbit around the sample with")
+Parser.add_argument("--Width", default=2048, help="Sets the width in pixels of the output images")
+Parser.add_argument("--Height", default=2048, help="Sets the height in pixels of the output images")
+Parser.add_argument("--FOV", default=2048, help="Sets the FOV of the camera")
+Parser.add_argument("--BatchDownload", default=2, help="Sets the number of images to download simultaneously")
 Args = Parser.parse_args()
+
+
+
+def PointsInCircum(r, n=100):
+    return [(math.cos(2*math.pi/n*x)*r,math.sin(2*math.pi/n*x)*r) for x in range(0,n+1)]
 
 
 def AddSCCompartmnet(_Sim, _Shape):
@@ -75,7 +86,27 @@ def Main(Args):
     MySim = ClientInstance.LoadSimulation(SimulationHandle)
 
     print(" - Visualizing Simulation")
+    VisualizerJob = BrainGenix.NES.Visualizer.Configuration()
+    VisualizerJob.ImageWidth_px = Args.Width
+    VisualizerJob.ImageHeight_px = Args.Height
 
+
+    # Render In Circle Around Sim
+    Radius = Args.CameraDistance
+    Steps = Args.NumSteps
+    ZHeight = Args.CameraHeight
+
+    for Point in PointsInCircum(Radius, Steps):
+
+        VisualizerJob.CameraFOVList_deg.append(Args.FOV)
+        VisualizerJob.CameraPositionList_um.append([Point[0], Point[1], ZHeight])
+        VisualizerJob.CameraLookAtPositionList_um.append([0, 0, ZHeight])
+
+    Visualizer = MySim.SetupVisualizer()
+    Visualizer.GenerateVisualization(VisualizerJob)
+
+
+    Visualizer.SaveImages(f"{OutputFilePath}/", Args.BatchDownload)
 
 if __name__ == "__main__":
     Main(Args)
