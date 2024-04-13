2fbda0f4cf3dcb132d7ef6431a3c91178e54c82f
Thomas Liao <tliao@carboncopies.org>
2024-04-12 18:21:59 -0700

    Add neuroglancer tests

---
+++ NeuroglancerTests/Neuroglancer.py
@@ -0,0 +1,17 @@
+import neuroglancer
+
+ip = '0.0.0.0' # or public IP of the machine for sharable display
+port = 9999 # change to an unused port number
+neuroglancer.set_server_bind_address(bind_address=ip, bind_port=port)
+
+viewer = neuroglancer.Viewer()
+
+with viewer.txn() as s:
+    s.layers['image'] = neuroglancer.ImageLayer(source='precomputed://http://localhost:8000/out')
+    # s.layers['segmentation'] = neuroglancer.SegmentationLayer(source='precomputed://gs://neuroglancer-janelia-flyem-hemibrain/v1.0/segmentation', selected_alpha=0.3)
+
+print(viewer)
+
+
+while True:
+    pass
