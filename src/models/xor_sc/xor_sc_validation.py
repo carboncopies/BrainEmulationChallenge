670f47737aac8107c30413bffb15b12053c46cb0
Thomas Liao <tliao@carboncopies.org>
2024-04-29 13:47:11 -0700

    Create Report Generator

--- src/models/xor_sc/xor_sc_validation.py
+++ src/models/xor_sc/xor_sc_validation.py
@@ -22,23 +22,18 @@ import BrainGenix.NES as NES
 import BrainGenix.EVM as EVM
 import BrainGenix
 
-savefolder = 'output/'+str(datetime.now()).replace(":", "_")+'-acquisition'
+
+# 0: Setup Output Report
+SaveFolder = 'ValidationReports/'+str(datetime.now()).replace(":", "_")+'-acquisition'
+
 
 # 1. Init NES connection
 
 # Handle Arguments for Host, Port, etc
-Parser = argparse.ArgumentParser(
-    description="BrainGenix-API Simple Python Validation Test Script"
-)
-Parser.add_argument(
-    "-Host", default="localhost", type=str, help="Host to connect to"
-)
-Parser.add_argument(
-    "-Port", default=8000, type=int, help="Port number to connect to"
-)
-Parser.add_argument(
-    "-UseHTTPS", default=False, type=bool, help="Enable or disable HTTPS"
-)
+Parser = argparse.ArgumentParser(description="BrainGenix-API Simple Python Validation Test Script")
+Parser.add_argument("-Host", default="localhost", type=str, help="Host to connect to")
+Parser.add_argument("-Port", default=8000, type=int, help="Port number to connect to")
+Parser.add_argument("-UseHTTPS", default=False, type=bool, help="Enable or disable HTTPS")
 Args = Parser.parse_args()
 
 # Start Tests
@@ -67,6 +62,75 @@ with open('./.SimulationHandle', 'r') as f:
 with open('./.EmulationHandle', 'r') as f:
     EMUSaveName = f.read()
 
-Result = EVM.Validation.SCValidation(ClientInstance, KTGSaveNAme, EMUSaveName)
-print(Result)
+print(" -- Running Edit Metrics")
+EditJSON = EVM.Validation.SCValidation(ClientInstance, KTGSaveNAme, EMUSaveName)
+
+print(" -- Generating Report Information")
+os.makedirs(SaveFolder + "/JSON")
+
+print(" -- Creating Markdown Report")
+
+EditListMarkdown = ""
+GraphEditListJSON = EditJSON["GraphEdits"]
+for i in range(len(GraphEditListJSON)):
+    Edit = GraphEditListJSON[i]
+    EditListMarkdown += f"""
+##### Edit {i}  
+- Cost: {Edit["Cost"]}  
+- Data: {Edit["Data"]}  
+- Operation: {Edit["Op"]}  
+
+"""
+
+Report:str = f"""
+# WBE Standardized Challenge - Submission Report
+
+
+
+## Report Summary
+
+### Structural Metrics
+
+#### Edit Distance
+ - Score: {EditJSON["Scores"]["GraphEditDistanceScore"]}
+ - RawCost: {EditJSON["Scores"]["GraphEditRawCost"]}
+ - NumElements: {EditJSON["Scores"]["NumElements"]}
+
+ 
+### Functional Metrics
+
+Coming soon!
+
+
+
+## Detailed Information
+
+### Structural Metrics
+
+#### Edit Distance
+See `JSON/EditDistance.json` for detailed information. 
+{EditListMarkdown}
+
+
+
+## Legend
+
+### Graph Edit Operations
+The enumerated ops (top is 0):  
+- vertex_insertion = 0,  
+- vertex_deletion = 1,  
+- vertex_substitution = 2,  
+- edge_insertion = 3,  
+- edge_deletion = 4,  
+- edge_substitution = 5,  
+
+
+"""
+with open(f"{SaveFolder}/Report.md", "w") as f:
+    f.write(Report)
+
+print(" -- Saving Edit Distance JSON Info")
+with open(f"{SaveFolder}/JSON/EditDistance.json", "w") as f:
+    f.write(json.dumps(EditJSON))
+
 
