de7c9837a3485cf8c316e8c82d0d4fe6d630ad94
Randal A. Koene <rkoene@carboncopies.org>
2024-06-23 22:09:16 -0700

    Automatically set the bevel depth.

--- src/models/xor_scnm/objcurves.py
+++ src/models/xor_scnm/objcurves.py
@@ -1,5 +1,8 @@
 # Blender Python API script used to convert OBJ lines into curves
 # to give them material with color.
+# In Blender 4.0, OBJ lines already appear to be loaded as curves.
+# Either that, or all the objects imported were still selected
+# when conversion was carried out.
 
 import bpy
 
@@ -12,5 +15,7 @@ first_axon.select_set(True)
 bpy.context.view_layer.objects.active = first_axon
 
 bpy.ops.object.convert(target='CURVE')
+
+first_axon.data.bevel_depth = 0.1
 
 bpy.ops.wm.save_as_mainfile(filepath='test.blend')
