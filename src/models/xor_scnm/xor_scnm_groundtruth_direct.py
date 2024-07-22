bea981e2f5bf88a0b5d0fc2a69d9e0f451664222
Randal A. Koene <rkoene@carboncopies.org>
2024-07-22 16:27:06 -0700

    Added OBJ and Blend options.

--- src/models/xor_scnm/xor_scnm_groundtruth_direct.py
+++ src/models/xor_scnm/xor_scnm_groundtruth_direct.py
@@ -23,13 +23,36 @@ Parser.add_argument("-Port", default=8000, type=int, help="Port number to connec
 Parser.add_argument("-UseHTTPS", default=False, type=bool, help="Enable or disable HTTPS")
 Parser.add_argument("-modelfile", type=str, help="File to read model instructions from")
 Parser.add_argument("-modelname", default="xor_scnm", type=str, help="Name of neuronal circuit model to save")
+Parser.add_argument("-DoOBJ", default=False, type=bool, help="Netmorph should produce OBJ output")
+Parser.add_argument("-DoBlend", default=False, type=bool, help="Netmorph should produce Blender output")
+Parser.add_argument("-BlendExec", default="/home/rkoene/blender-4.1.1-linux-x64/blender", type=str, help="Path to Blender executable")
+Parser.add_argument("-BevelDepth", default=0.1, type=float, help="Blender neurite bevel depth")
 Args = Parser.parse_args()
+
+if Args.DoBlend:
+    Args.DoOBJ = True
 
 modelcontent = 'kjhskdjfhkjhs'
 
 if Args.modelfile:
     with open(Args.modelfile, 'r') as f:
         modelcontent = f.read()
+
+NETMORPH_OBJ = '''
+outattr_make_full_OBJ=true;
+outattr_OBJ_bevdepth_axon=%.1f;
+outattr_OBJ_bevdepth_dendrite=%.1f;
+'''
+
+NETMORPH_BLEND = '''
+outattr_make_full_blend=true;
+blender_exec_path=%s;
+'''
+
+if Args.DoOBJ:
+    modelcontent += NETMORPH_OBJ % (Args.BevelDepth, Args.BevelDepth)
+if Args.DoBlend:
+    modelcontent += NETMORPH_BLEND % Args.BlendExec
 
 # Create Client Configuration For Local Simulation
 print(" -- Creating Client Configuration For Local Simulation")
