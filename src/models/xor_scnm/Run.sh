8c487b51e4585aa774691b4face653e28c9d4892
Thomas Liao <tliao@carboncopies.org>
2024-10-30 17:05:08 -0700

    Add Run Script For Simplicity

---
+++ src/models/xor_scnm/Run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "This script expects an argument which is the netmorph model file to run (this message shows if you have or have not given one)"
+
+./xor_scnm_groundtruth_reservoir.py -modelname "$1"
+./xor_scnm_groundtruth_connectome.py
+
+./xor_scnm_acquisition_direct.py -RenderEM -NoDownloadEM
+
