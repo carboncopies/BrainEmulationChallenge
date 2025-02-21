bfb51ec657f7c9de32d0ad40eb69136c1d92b414
Thomas Liao <tliao@carboncopies.org>
2025-02-20 20:37:15 -0800

    update run script

--- src/models/xor_scnm/xor_scnm_acquisition_direct.py
+++ src/models/xor_scnm/xor_scnm_acquisition_direct.py
@@ -490,9 +490,11 @@ if (Args.RenderEM):
     EMConfig.PostBlurNoisePasses = 1
     EMConfig.PreBlurNoisePasses = 0
     EMConfig.TearingEnabled = False
-    EMConfig.GenerateImageNoise = False
-    EMConfig.EnableGaussianBlur = False
-    EMConfig.EnableInterferencePattern = False
+    EMConfig.RenderBorders = True
+    EMConfig.GeneratePerlinNoise = True
+#    EMConfig.GenerateImageNoise = False
+#    EMConfig.EnableGaussianBlur = False
+#    EMConfig.EnableInterferencePattern = False
     VSDAEMInstance = MySim.AddVSDAEM(EMConfig)
 
 
