f0d1680bcfbfb04503119b2a9264a02a31a74c18
Thomas Liao <tliao@carboncopies.org>
2025-02-27 18:02:56 -0800

    Update script

--- src/models/xor_scnm/Run.sh
+++ src/models/xor_scnm/Run.sh
@@ -11,5 +11,5 @@ fi
 ./xor_scnm_groundtruth_reservoir.py -modelfile "$1"
 ./xor_scnm_groundtruth_connectome.py
 
-./xor_scnm_acquisition_direct.py -RenderEM -NoDownloadEM -SubdivideSize 10 -Neuroglancer -Resolution_um 0.1
+./xor_scnm_acquisition_direct.py -RenderEM -NoDownloadEM -SubdivideSize 7 -Neuroglancer -Resolution_um 0.1
 
