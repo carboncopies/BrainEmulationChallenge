8edba4725f74955820054639351ec5514a369a4e
rkoene <rkoene@carboncopies.org>
2025-03-22 16:12:04 -0700

    Added some more arguments to the Run.sh script.

--- src/models/xor_scnm/Run.sh
+++ src/models/xor_scnm/Run.sh
@@ -1,15 +1,41 @@
 #!/bin/bash
 
-echo "This script expects an argument which is the netmorph model file to run (this message shows if you have or have not given one)"
+echo "This script expects the following arguments:"
+echo "  1: subdivide size (default 10)"
+echo "  2: resolution (default 0.1 um)"
+echo "  3: model file name (default nesvbp-xor-res-sep-targets)"
+echo "  4: model name (default xor_scnm)"
 
-# Check if $1 is empty
-if [ -z "$1" ]; then
-  # Set $1 to a default value
-  set -- "nesvbp-xor-res-sep-targets"
+subdivide=$1
+resolution=$2
+modelfile=$3
+modelname=$4
+
+if [ "$subdivide" = "" ]; then
+  subdivide="10"
+fi
+if [ "$resolution" = "" ]; then
+  resolution="0.1"
+fi
+if [ "$modelfile" = "" ]; then
+  modelfile="nesvbp-xor-res-sep-targets"
+fi
+if [ "$modelname" = "" ]; then
+  modelname="xor_scnm"
 fi
 
-./xor_scnm_groundtruth_reservoir.py -modelfile "$1"
-./xor_scnm_groundtruth_connectome.py
+echo "Using:"
+echo "  subdivide : $subdivide"
+echo "  resolution: $resolution"
+echo "  modelfile : $modelfile"
+echo "  modelname : $modelname"
 
-./xor_scnm_acquisition_direct.py -RenderEM -NoDownloadEM -SubdivideSize 10 -Neuroglancer -Resolution_um 0.1
+# Use Netmorph to build the reservoir
+./xor_scnm_groundtruth_reservoir.py -modelfile "$modelfile" -modelname "$modelname"
+
+# Tune the synapses in the reservoir
+./xor_scnm_groundtruth_connectome.py -modelname "$modelname"
+
+# Acquire data sets
+./xor_scnm_acquisition_direct.py -modelname "$modelname-tuned" -RenderEM -NoDownloadEM -SubdivideSize "$subdivide" -Neuroglancer -Resolution_um "$resolution"
 
