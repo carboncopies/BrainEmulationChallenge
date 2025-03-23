837293074667e7d95b55e0a45b83315137ca1502
rkoene <rkoene@carboncopies.org>
2025-03-22 19:47:59 -0700

    Much more versatile Run.sh.

--- src/models/xor_scnm/Run.sh
+++ src/models/xor_scnm/Run.sh
@@ -1,41 +1,73 @@
 #!/bin/bash
 
-echo "This script expects the following arguments:"
-echo "  1: subdivide size (default 10)"
-echo "  2: resolution (default 0.1 um)"
-echo "  3: model file name (default nesvbp-xor-res-sep-targets)"
-echo "  4: model name (default xor_scnm)"
+# Default values
+executeat="r"
+subdivide="10"
+resolution="0.1"
+modelfile="nesvbp-xor-res-sep-targets"
+modelname="xor_scnm"
 
-subdivide=$1
-resolution=$2
-modelfile=$3
-modelname=$4
+# Function to display usage
+usage() {
+    echo "Usage: $0 [-x execute-at] [-s subdivide] [-r resolution] [-f modelfile] [-m modelname]"
+    echo "  -x  start executing at step (default r)"
+    echo "      options are:"
+    echo "        r - reservoir generation (Netmorph)"
+    echo "        c - connectome tuning"
+    echo "        a - acquisition of data set"
+    echo "  -s  subdivide size (default 10)"
+    echo "  -r  resolution (default 0.1 um)"
+    echo "  -f  model file name (default nesvbp-xor-res-sep-targets)"
+    echo "  -m  model name (default xor_scnm)"
+    exit 1
+}
 
-if [ "$subdivide" = "" ]; then
-  subdivide="10"
-fi
-if [ "$resolution" = "" ]; then
-  resolution="0.1"
-fi
-if [ "$modelfile" = "" ]; then
-  modelfile="nesvbp-xor-res-sep-targets"
-fi
-if [ "$modelname" = "" ]; then
-  modelname="xor_scnm"
-fi
+# Parse command line arguments
+while getopts ":s:r:f:m:x:" opt; do
+  case ${opt} in
+    x )
+      executeat=$OPTARG
+      ;;
+    s )
+      subdivide=$OPTARG
+      ;;
+    r )
+      resolution=$OPTARG
+      ;;
+    f )
+      modelfile=$OPTARG
+      ;;
+    m )
+      modelname=$OPTARG
+      ;;
+    \? )
+      echo "Invalid option: -$OPTARG" 1>&2
+      usage
+      ;;
+    : )
+      echo "Invalid option: -$OPTARG requires an argument" 1>&2
+      usage
+      ;;
+  esac
+done
+shift $((OPTIND -1))
 
 echo "Using:"
+echo "  execute-at: $executeat"
 echo "  subdivide : $subdivide"
 echo "  resolution: $resolution"
 echo "  modelfile : $modelfile"
 echo "  modelname : $modelname"
 
+if [ "$executeat" = "r" ]; then
 # Use Netmorph to build the reservoir
 ./xor_scnm_groundtruth_reservoir.py -modelfile "$modelfile" -modelname "$modelname"
+fi
 
+if [ "$executeat" = "r" -o "$executeat" = "c" ]; then
 # Tune the synapses in the reservoir
 ./xor_scnm_groundtruth_connectome.py -modelname "$modelname"
+fi
 
 # Acquire data sets
 ./xor_scnm_acquisition_direct.py -modelname "$modelname-tuned" -RenderEM -NoDownloadEM -SubdivideSize "$subdivide" -Neuroglancer -Resolution_um "$resolution"
-
