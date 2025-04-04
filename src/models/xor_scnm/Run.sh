61ea665029406c8cc9703d731411e71a31d75b84
rkoene <rkoene@carboncopies.org>
2025-04-03 21:27:52 -0700

    Added acquisition and Run.sh options to ease remote and on-server use of the front-end scripts.

--- src/models/xor_scnm/Run.sh
+++ src/models/xor_scnm/Run.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
 
+# Local front-end to remote API server options
+Rhost="pve.braingenix.org"
+Rport="8000"
+
 # Default values
 executeat="r"
 subdivide="10"
 resolution="0.1"
 modelfile="nesvbp-xor-res-sep-targets"
 modelname="xor_scnm"
+neuroglancer=""
+segmentation=""
+meshes=""
 simID=""
+host=""
+port=""
+forceURLbase=""
 
 # Function to display usage
 usage() {
-    echo "Usage: $0 [-x execute-at] [-s subdivide] [-r resolution] [-f modelfile] [-m modelname] [-i simID]"
+    echo "Usage: $0 [-R] [-x execute-at] [-s subdivide] [-r resolution] [-f modelfile] [-m modelname] [-n] [-S] [-M] [-F baseURL] [-i simID]"
+    echo "  -R  running locally to remote API server at $Rhost:$Rport"
     echo "  -x  start executing at step (default r)"
     echo "      options are:"
     echo "        r - reservoir generation (Netmorph)"
@@ -20,13 +31,22 @@ usage() {
     echo "  -r  resolution (default 0.1 um)"
     echo "  -f  model file name (default nesvbp-xor-res-sep-targets)"
     echo "  -m  model name (default xor_scnm)"
+    echo "  -n  Generate Neuroglancer data"
+    echo "  -S  Generate segmentation files"
+    echo "  -M  Generate mesh files"
+    echo "  -F  Force URL base of Neuroglancer URL (only needed if running"
+    echo "      this script on remote API server but browsing locally)"
     echo "  -i  reconnect with simID and (re)run acquisition"
     exit 1
 }
 
 # Parse command line arguments
-while getopts ":s:r:f:m:x:i:" opt; do
+while getopts "R:s:r:f:m:x:nSMF:i:" opt; do
   case ${opt} in
+    R )
+      host="-Host $Rhost"
+      port="-Port $Rport"
+      ;;
     x )
       executeat=$OPTARG
       ;;
@@ -41,6 +61,18 @@ while getopts ":s:r:f:m:x:i:" opt; do
       ;;
     m )
       modelname=$OPTARG
+      ;;
+    n )
+      neuroglancer="-Neuroglancer"
+      ;;
+    S )
+      segmentation="-Segmentation"
+      ;;
+    M )
+      meshes="-Meshes"
+      ;;
+    F )
+      forceURLbase="-NeuroglancerURLBase $OPTARG"
       ;;
     i )
       simID=$OPTARG
@@ -57,6 +89,10 @@ while getopts ":s:r:f:m:x:i:" opt; do
 done
 shift $((OPTIND -1))
 
+if [ "$host" != "" -a "$neuroglancer" != "" -a "$forceURLbase" == "" ]; then
+  forceURLbase="-NeuroglancerURLBase http://$Rhost:$Rport"
+fi
+
 if [ "$simID" = "" ]; then
   modelname="$USER-$modelname"
 else
@@ -66,27 +102,33 @@ else
 fi
 
 echo "Using:"
-echo "  execute-at: $executeat"
-echo "  subdivide : $subdivide"
-echo "  resolution: $resolution"
-echo "  modelfile : $modelfile"
-echo "  modelname : $modelname"
-echo "  simID     : $simID"
+echo "  execute-at  : $executeat"
+echo "  subdivide   : $subdivide"
+echo "  resolution  : $resolution"
+echo "  modelfile   : $modelfile"
+echo "  modelname   : $modelname"
+echo "  neuroglancer: $neuroglancer"
+echo "  segmentation: $segmentation"
+echo "  meshes      : $meshes"
+echo "  simID       : $simID"
+echo "  host        : $host"
+echo "  port        : $port"
+echo "  forceURLbase: $forceURLbase"
 
 if [ "$executeat" = "r" ]; then
 # Use Netmorph to build the reservoir
-./xor_scnm_groundtruth_reservoir.py -modelfile "$modelfile" -modelname "$modelname"
+./xor_scnm_groundtruth_reservoir.py $host $port -modelfile "$modelfile" -modelname "$modelname"
 fi
 
 if [ "$executeat" = "r" -o "$executeat" = "c" ]; then
 # Tune the synapses in the reservoir
-./xor_scnm_groundtruth_connectome.py -modelname "$modelname"
+./xor_scnm_groundtruth_connectome.py $host $port -modelname "$modelname"
 fi
 
 if [ "$simID" = "" ]; then
   # Acquire data sets
-  ./xor_scnm_acquisition_direct.py -modelname "$modelname-tuned" -RenderEM -NoDownloadEM -SubdivideSize "$subdivide" -Neuroglancer -Resolution_um "$resolution"
+  ./xor_scnm_acquisition_direct.py $host $port -modelname "$modelname-tuned" -RenderEM -NoDownloadEM -SubdivideSize "$subdivide" $neuroglancer $segmentation $meshes $forceURLbase -Resolution_um "$resolution"
 else
   # Acquire data sets by rerunning a simulation that is still in NES server memory
-  ./xor_scnm_acquisition_direct.py -simID "$simID" -RenderEM -NoDownloadEM -SubdivideSize "$subdivide" -Neuroglancer -Resolution_um "$resolution"
+  ./xor_scnm_acquisition_direct.py $host $port -simID "$simID" -RenderEM -NoDownloadEM -SubdivideSize "$subdivide" $neuroglancer $segmentation $meshes $forceURLbase -Resolution_um "$resolution"
 fi
