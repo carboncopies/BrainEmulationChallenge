5ad785d3db9c67c46fd0084881d68170494b8871
Randal A. Koene <rkoene@carboncopies.org>
2026-04-23 12:03:02 -0700

    Prettier output.

--- src/models/autoassociative/inspect_batch_models.py
+++ src/models/autoassociative/inspect_batch_models.py
@@ -4,6 +4,7 @@
 # generated in a batch of samples by loading, inspecting, deleting simulations.
 
 import argparse
+import tqdm
 
 import vbpcommon as vbp
 from BrainGenix.BG_API import NES
@@ -17,6 +18,27 @@ def get_Args():
     Parser.add_argument("-UseHTTPS", default=False, type=bool, help="Enable or disable HTTPS")
     return Parser.parse_args()
 
+# Status bar helper functions
+def prepare_statusbar():
+    StatusBar = tqdm.tqdm("Progress", total=1)
+    StatusBar.leave = True
+    StatusBar.bar_format = "{desc}{percentage:3.0f}%|{bar}| [{elapsed}<{remaining}, {rate_fmt}{postfix}]"
+    StatusBar.colour = "green"
+    return StatusBar
+
+def update_statusbar(StatusBar, batchinfo:dict, numdone:int):
+    grand_total = len(batchinfo)
+
+    StatusBar.total = 100
+    if grand_total <= 0:
+        StatusBar.n = 100
+    else:
+        StatusBar.n = 100.0*numdone/grand_total
+    StatusBar.refresh()
+
+def close_statusbar(StatusBar):
+    update_statusbar(StatusBar)
+    StatusBar.close()
 
 # ===== Start of program steps
 if __name__ == '__main__':
@@ -31,11 +53,14 @@ if __name__ == '__main__':
 
     print('Number of completed samples: %d' % len(batchrun.batchinfo))
 
+    statusbar = prepare_statusbar()
+    num_neurons = []
+    numdone = 0
     for sample in batchrun.batchinfo.values():
         saved_model_name = sample['modelname']
 
         # Create A New Simulation
-        print(" -- Creating Simulation")
+        #print(" -- Creating Simulation")
         SimulationCfg = NES.Simulation.Configuration()
         SimulationCfg.Name = "Sample-"+saved_model_name
         SimulationCfg.Seed = 0
@@ -48,7 +73,7 @@ if __name__ == '__main__':
         # Load previously generated model
         try:
             MySim.ModelLoad(saved_model_name)
-            print("Loaded neuronal circuit model "+saved_model_name)
+            #print("Loaded neuronal circuit model "+saved_model_name)
         except:
             print('NES error: model load failed')
             exit(1)
@@ -62,7 +87,7 @@ if __name__ == '__main__':
 
         soma_centers = cell_positions['SomaCenters']
 
-        print('Number of neurons: %d' % len(soma_centers))
+        num_neurons.append(len(soma_centers))
 
         # Free memory
         try:
@@ -70,6 +95,13 @@ if __name__ == '__main__':
         except:
             print('NES error: unable to delete resident simulation')
             exit(1)
+
+        numdone += 1
+        update_statusbar(statusbar, batchrun.batchinfo, numdone)
+
+    close_statusbar(statusbar)
+
+    print('Number of neurons in each sample:\n'+str(num_neurons))
 
     print('Done.')
     exit(0)
