3d9dfb1bd21111f6d64fcfee9db6ab7eb7520345
yasufumi <52214705+yasufumi-nakata@users.noreply.github.com>
2026-05-16 12:12:13 +0900

    Use merged calcium imaging specs

--- src/components/prototyping/Calcium_Imaging.py
+++ src/components/prototyping/Calcium_Imaging.py
@@ -68,12 +68,12 @@ class Calcium_Imaging:
         }
         self.specs.update(specs)
 
-        self.id = specs['id']
-        self.fluorescing_neurons = specs['fluorescing_neurons']
-        self.calcium_indicator = specs['calcium_indicator']
-        self.indicator_rise_ms = specs['indicator_rise_ms']
-        self.indicator_decay_ms = specs['indicator_decay_ms']
-        self.indicator_interval_ms = specs['indicator_interval_ms']
+        self.id = self.specs['id']
+        self.fluorescing_neurons = self.specs['fluorescing_neurons']
+        self.calcium_indicator = self.specs['calcium_indicator']
+        self.indicator_rise_ms = self.specs['indicator_rise_ms']
+        self.indicator_decay_ms = self.specs['indicator_decay_ms']
+        self.indicator_interval_ms = self.specs['indicator_interval_ms']
         #self.microscope_lensfront_position_um = specs['microscope_lensfront_position_um']
         #self.microscope_rear_position_um = specs['microscope_rear_position_um']
         self.system_ref = system_ref
